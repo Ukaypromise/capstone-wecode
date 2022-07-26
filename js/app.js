5f003b0e4eebe5a237f1e71ac055296033f1780e
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-07-26 22:52:52 +0100

    Fix linter errors

--- js/app.js
+++ js/app.js
@@ -1,79 +1,78 @@
 // Navigation
-const hambugerMenu = document.querySelector(".hambuger");
+const hambugerMenu = document.querySelector('.hambuger');
 
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector('.nav-menu');
 
-const navLinks = document.querySelectorAll(".nav-link");
+const navLinks = document.querySelectorAll('.nav-link');
 
 // Toggle the menu
-hambugerMenu.addEventListener("click", () => {
-  hambugerMenu.classList.toggle("active");
+hambugerMenu.addEventListener('click', () => {
+  hambugerMenu.classList.toggle('active');
 
-  navMenu.classList.toggle("active");
+  navMenu.classList.toggle('active');
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    hambugerMenu.classList.remove("active");
+  link.addEventListener('click', () => {
+    hambugerMenu.classList.remove('active');
 
-    navMenu.classList.remove("active");
+    navMenu.classList.remove('active');
   });
 });
 
 const speakers = [
   {
     id: 1,
-    img: "../images/speakers/speaker1.png",
-    name: "Abubakar Siddiq Ango",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker1.png',
+    name: 'Abubakar Siddiq Ango',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
   {
     id: 2,
-    img: "../images/speakers/speaker2.png",
-    name: "Simi Oluwatomi",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker2.png',
+    name: 'Simi Oluwatomi',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
   {
     id: 3,
-    img: "../images/speakers/speaker3.png",
-    name: "Abdul Hakim",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker3.png',
+    name: 'Abdul Hakim',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
   {
     id: 4,
-    img: "../images/speakers/speaker4.png",
-    name: "Hayatu Abubakar",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker4.png',
+    name: 'Hayatu Abubakar',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
   {
     id: 5,
-    img: "../images/speakers/speaker5.png",
-    name: "Auwal MS",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker5.png',
+    name: 'Auwal MS',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
   {
     id: 6,
-    img: "../images/speakers/speaker6.png",
-    name: "Authentic One",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: '../images/speakers/speaker6.png',
+    name: 'Authentic One',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Faciliiusto. Cumque repellat delectus earum aliquid',
   },
 ];
 
-window.addEventListener("DOMContentLoaded", () => {
-  const displaySpeakers = speakers.map((speaker) => {
-    return `
+window.addEventListener('DOMContentLoaded', () => {
+  const displaySpeakers = speakers.map((speaker) => `
     <div id=${speaker.id} >
       <div class="speaker-container">
             <img
@@ -90,20 +89,18 @@ window.addEventListener("DOMContentLoaded", () => {
             </div>
         </div>
     </div>
-    `;
-  });
-  document.getElementById("wrapper").innerHTML = displaySpeakers.join("");
+    `);
+  document.getElementById('wrapper').innerHTML = displaySpeakers.join('');
 });
 
 // See More
-const seeMore = document.querySelector(".see-more");
-const seeMoreContent = document.querySelector(".wrapper");
-console.log(seeMoreContent);
+const seeMore = document.querySelector('.see-more');
+const seeMoreContent = document.querySelector('.wrapper');
 
-seeMore.addEventListener("click", () => {
-  if (seeMoreContent.style.display == "block") {
-    seeMoreContent.style.display = "none";
+seeMore.addEventListener('click', () => {
+  if (seeMoreContent.style.display === 'block') {
+    seeMoreContent.style.display = 'none';
   } else {
-    seeMoreContent.style.display = "block";
+    seeMoreContent.style.display = 'block';
   }
 });
